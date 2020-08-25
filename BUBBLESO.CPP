b13b444f0dca0faf720480917865c471875653c4
shivgopal07 <56026986+shivgopal07@users.noreply.github.com>
2020-08-25 13:15:57 +0530

    Add files via upload

---
+++ BUBBLESO.CPP
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int array[100],n,c,d,swap;
+	printf("enter no of elements\n");
+	scanf("%d",&n);
+	printf("enter %din integer\n",n);
+
+	for(c=0;c<n;c++)
+		scanf("%d", &array[c]);
+	for(c=0;c<n-1;c++)
+	{
+		for(d=0;d<n-c-1;d++)
+		{
+			if(array[d]>array[d+1])
+			{
+			swap=array[d];
+			array[d]=array[d+1];
+			array[d+1]=swap;
+			}
+		}
+	}
+	printf("sorted list in acsending order:\n");
+
+	for(c=0;c<n;c++)
+		printf("%d\n",array[c]);
+	return 0;
+}
